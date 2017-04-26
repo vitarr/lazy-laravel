538644b58af0301df12b48b5f112133d4f8a1359
sasha44 <12345@i.ua>
2017-04-26 17:50:49 +0300

    some work with laravel

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,45 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use App\Task;
+use Illuminate\Http\Request;
 
+/**
+ * Вывести панель с задачами
+ */
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    return view('tasks', [
+        'tasks' => $tasks,
+        'title' => 'all tasks'
+    ]);
+});
+
+/**
+ * Добавить новую задачу
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+                'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+                        ->withInput()
+                        ->withErrors($validator);
+    }
+
+    // Создание задачи...
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * Удалить задачу
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+    return redirect('/');
 });
---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,13 @@
+<!-- resources/views/common/errors.blade.php -->
+
+@if (count($errors) > 0)
+<!-- Список ошибок формы -->
+<div class="alert alert-danger">
+    <strong>Упс! Что-то пошло не так!</strong>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
